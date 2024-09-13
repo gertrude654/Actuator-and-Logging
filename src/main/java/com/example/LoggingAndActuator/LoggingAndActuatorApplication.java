977c48d1b364b55a13c5ab20caf9d5a26966717c
gertrude654 <gertrude.uzimana@amalitechtraining.org>
2024-09-13 15:25:13 +0200

    setup project for  actuator and logging labs

---
+++ src/main/java/com/example/LoggingAndActuator/LoggingAndActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.LoggingAndActuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingAndActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggingAndActuatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LoggingAndActuator/LoggingAndActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LoggingAndActuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingAndActuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
